16ad00e6ec366d4d7c5278c2b2536fb31838014f
bsnabil <nabil.bensalem22222@gmail.com>
2025-01-29 10:02:47 +0100

    test applicaiton

---
+++ apps/python-app/main.py
@@ -0,0 +1,37 @@
+# Import Libraries
+import os
+import numpy as np
+import shap
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LinearRegression
+import mlflow
+from mlflow.artifacts import download_artifacts
+from mlflow.tracking import MlflowClient
+
+# Prepare the Training Data
+X, y = load_diabetes(return_X_y=True, as_frame=True)
+X = X.iloc[:50, :4]
+y = y.iloc[:50]
+
+# Train a model
+model = LinearRegression()
+model.fit(X, y)
+
+# Log an explanation
+with mlflow.start_run() as run:
+    mlflow.shap.log_explanation(model.predict, X)
+
+# List Artifacts
+client = MlflowClient()
+artifact_path = "model_explanations_shap"
+artifacts = [x.path for x in client.list_artifacts(run.info.run_id, artifact_path)]
+print("# artifacts:")
+print(artifacts)
+
+# Load the logged explanation
+dst_path = download_artifacts(run_id=run.info.run_id, artifact_path=artifact_path)
+base_values = np.load(os.path.join(dst_path, "base_values.npy"))
+shap_values = np.load(os.path.join(dst_path, "shap_values.npy"))
+
+# Show a Force Plot
+shap.force_plot(float(base_values), shap_values[0, :], X.iloc[0, :], matplotlib=True)
