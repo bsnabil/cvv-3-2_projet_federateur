07f15b4d375b4a52c6a5e4dd18257aa40871191c
BEN SALEM Nabil <125509526+bsnabil@users.noreply.github.com>
2025-02-28 12:06:51 +0100

    mise en place app.py

--- apps/mlops/mlflow/run-app.sh
+++ apps/mlops/mlflow/run-app.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-export MLFLOW_EXPERIMENT_NAME='my-sample-experiment'
-export MLFLOW_TRACKING_URI='http://192.168.56.7' # change port
 
 python3 -m pip install --upgrade pip
 
-#Create python env
+#Create python venv
 python3 -m venv .venv
 source .venv/bin/activate
+#install dependencies
+pip install mlflow scikit-learn shap matplotlib
+sleep 200
+export MLFLOW_EXPERIMENT_NAME='my-sample-experiment'
+export MLFLOW_TRACKING_URI='http://192.168.56.7' # change port
